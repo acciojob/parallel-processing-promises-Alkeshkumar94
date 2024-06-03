f937fcf3bb7b1dcd694e38c9763604c1af2c3753
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-06-03 08:34:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+btn.addEventListener("click", () => {
+  downloadImages(images)
+    .then(downloadedImages => {
+      output.innerHTML = ''; // Clear any previous images
+      downloadedImages.forEach(img => output.appendChild(img));
+    })
+    .catch(error => {
+      console.error(error);
+      alert(error);
+    });
+});
+
+function downloadImages(imageUrls) {
+  const imagePromises = imageUrls.map(imageObj => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = imageObj.url;
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(`Failed to load image's URL: ${imageObj.url}`);
+    });
+  });
+
+  return Promise.all(imagePromises);
+}
